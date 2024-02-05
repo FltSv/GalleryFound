8fb8e4cd6d39d11083d2d343d999bc7806e8a2ce
Suna <sc.flute495@gmail.com>
2024-02-05 09:07:13 +0900

    #33 TypeScript構成

--- Hosting/webpack.config.js
+++ Hosting/webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',                  // 変換元のエントリーポイントファイルを指定します。
-    output: {
-      path: path.resolve(__dirname, 'public'),  // 変換したファイルの出力先フォルダパスと、
-      filename: 'bundle.js'                   // 出力するファイル名を指定します。
-    },
-    devtool: 'eval-source-map',
-  }
+  mode: 'development',
+  entry: './src/index.js',                  // 変換元のエントリーポイントファイルを指定します。
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
+    ],
+  },
+  // import 文で .ts ファイルを解決する
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),  // 変換したファイルの出力先フォルダパスと、
+    filename: 'bundle.js'                   // 出力するファイル名を指定します。
+  },
+  devtool: 'eval-source-map',
+}
