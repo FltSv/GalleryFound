746b1a6e26eb50dfc15f836554dacc28fe240bb8
nozomi <onozomi0708@gmail.com>
2024-03-16 17:24:07 +0900

    header-change

--- Hosting/src/components/ui/Logo.tsx
+++ Hosting/src/components/ui/Logo.tsx
@@ -1,9 +1,32 @@
-const Logo = () => {
+const Header = () => {
   return (
     <h1 className="text-7xl mb-4 no-underline font-dancing">
       <a href="/">Gallery Found</a>
     </h1>
+    <header>
+
+    <a
+      id="header-login-button"
+      href="/login"
+      class="rounded-button"
+      style="display: none"
+      >Creator Login</a
+    >
+    <a
+      id="header-mypage-button"
+      href="/mypage"
+      class="rounded-button"
+      style="display: none"
+      >MyPage</a
+    > <a
+        id="header-logout-button"
+        href="/"
+        class="rounded-button"
+        style="display: none"
+        >Logout</a
+      >
+    </header>
   );
 };
 
-export default Logo;
+export default Header;
