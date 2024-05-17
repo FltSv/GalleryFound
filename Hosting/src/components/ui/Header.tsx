fdebe121f5a44d28b5e074500a930b896d16c57f
Suna <sc.flute495@gmail.com>
2024-05-17 10:41:43 +0900

    #69 Header/Logo周辺の修正

--- Hosting/src/components/ui/Header.tsx
+++ Hosting/src/components/ui/Header.tsx
@@ -1,39 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { signOut } from '../../Auth';
 import { useAuthContext } from '../AuthContext';
 
 const Header = () => {
   const styles =
-    'px-4 py-2 mx-1 my-0 border border-solid border-neutral-950 rounded-3xl text-neutral-950 no-underline transition hover:bg-sky-200';
+    'px-4 py-2 border border-solid border-current rounded-3xl no-underline transition hover:bg-sky-200 hover:bg-opacity-60';
 
   const { user, loading } = useAuthContext();
-
-  if (loading) {
-    return <></>;
-  }
+  const location = useLocation();
 
   const isLogged = user !== null;
+
+  const visibleLoginButton = !isLogged && location.pathname !== '/login';
+  const visibleMypage = isLogged && location.pathname !== '/mypage';
+  const visibleLogout = isLogged;
 
   const onSignOut = () => {
     void signOut();
   };
 
   return (
-    <header className="flex justify-end items-center px-5 py-3">
-      {!isLogged && (
-        <Link to="login" className={styles}>
-          Creator Login
-        </Link>
-      )}
-      {isLogged && (
-        <Link to="/mypage" className={styles}>
-          MyPage
-        </Link>
-      )}
-      {isLogged && (
-        <Link onClick={onSignOut} to="/" className={styles}>
-          Logout
-        </Link>
+    <header className="mb-4 p-4">
+      <div className="inline-block">
+        <h1 className="mb-4 font-magneto text-5xl">
+          <Link to="/">Gallery Found</Link>
+        </h1>
+      </div>
+      {loading ? (
+        <></>
+      ) : (
+        <div className="float-right inline-block">
+          <div className="flex gap-4">
+            {visibleLoginButton && (
+              <Link to="login" className={styles}>
+                Creator Login
+              </Link>
+            )}
+            {visibleMypage && (
+              <Link to="/mypage" className={styles}>
+                MyPage
+              </Link>
+            )}
+            {visibleLogout && (
+              <Link onClick={onSignOut} to="/" className={styles}>
+                Logout
+              </Link>
+            )}
+          </div>
+        </div>
       )}
     </header>
   );
