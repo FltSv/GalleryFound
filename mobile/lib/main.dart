410356941f0bd9673e18b3ad7d7b1c32ffd3b0b9
Suna <sc.flute495@gmail.com>
2024-07-09 12:34:36 +0900

    #17 トップ画面の実装

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gallery Found',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,95 +32,82 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final screenSize = MediaQuery.of(context).size;
+    final centerX = screenSize.width / 2;
+    final centerY = screenSize.height / 2;
+    final buttonSize = screenSize.width * 0.1;
+    final radius = screenSize.width * 0.28; // ボタンの配置半径
+
+    //todo ボタン押下時の動作
+    booksFunc() => {};
+    mapFunc() => {};
+    exhibitsFunc() => {};
+    creatorsFunc() => {};
+
+    final List<ButtonProp> props = [
+      ButtonProp(Icons.menu_book, Colors.green, booksFunc),
+      ButtonProp(Icons.location_on, Colors.blue, mapFunc),
+      ButtonProp(Icons.brush, Colors.red, exhibitsFunc),
+      ButtonProp(Icons.person, Colors.purple, creatorsFunc),
+    ];
+
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          color: Color(0x413c4300),
+          image: DecorationImage(
+            image: AssetImage('assets/palette_background.png'),
+          ),
+        ),
+        child: Stack(
+          children: props.asMap().entries.map((entry) {
+            final index = entry.key;
+            final prop = entry.value;
+
+            final angle = (2 * pi * index) / props.length;
+            final x = centerX + radius * cos(angle);
+            final y = centerY + radius * sin(angle);
+
+            return _iconButton(prop, x, y, buttonSize);
+          }).toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _iconButton(ButtonProp prop, double x, double y, double size) {
+    return Positioned(
+      left: x - size,
+      top: y - size,
+      child: IconButton.filled(
+        icon: Icon(prop.icon),
+        iconSize: size,
+        padding: EdgeInsets.all(size * 0.5),
+        style: IconButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: prop.color,
+        ),
+        onPressed: prop.onTap,
+      ),
+    );
+  }
+}
+
+class ButtonProp {
+  final IconData icon;
+  final Color color;
+  final void Function() onTap;
+
+  ButtonProp(this.icon, this.color, this.onTap);
 }
