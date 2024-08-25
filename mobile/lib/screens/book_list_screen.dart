89e5a9a2b2d8176a8d8603e50e3f6648f49f944a
himarori <mike1529saxgirl@gmail.com>
2024-08-25 13:11:28 +0900

    #105 ダミーデータをFakerepoへ分離

--- mobile/lib/screens/book_list_screen.dart
+++ mobile/lib/screens/book_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:mobile/models/book.dart';
+import 'package:mobile/providers/data_provider.dart';
 import 'package:mobile/providers/navigate_provider.dart';
 import 'package:mobile/screens/book_detail_screen.dart';
 import 'package:mobile/widgets/empty_state.dart';
@@ -13,42 +14,6 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
-  final List<Book> books = [
-    Book(
-        id: '1',
-        title: 'ひまわり',
-        image:
-            'https://media.mstdn.jp/accounts/avatars/110/989/102/133/377/139/original/ef63d785a819a2f2.png',
-        urls: ['https://mstdn.jp/@himarori']),
-    Book(
-        id: '2',
-        title: '砂ちゃん',
-        image:
-            'https://media.mstdn.jp/accounts/avatars/000/113/775/original/4a94c289c389d678.jpg',
-        urls: ['https://mstdn.jp/@fltsv', 'https://mstdn.jp/@himarori']),
-    Book(
-        id: '3',
-        title: 'よるねこ',
-        image:
-            'https://media.mstdn.jp/accounts/avatars/000/155/200/original/2e948193ee954e55428290ad6ecada7f.png',
-        urls: [
-          'https://mstdn.jp/@NightCat',
-          'https://mstdn.jp/@himarori',
-          'https://mstdn.jp/@fltsv'
-        ]),
-    Book(
-        id: '4',
-        title: 'もやちゃ',
-        image:
-            'https://media.mstdn.jp/accounts/avatars/109/719/600/512/825/943/original/898331de566f6f5e.png',
-        urls: [
-          'https://mstdn.jp/@kisskamakiri',
-          'https://mstdn.jp/@fltsv',
-          'https://mstdn.jp/@NightCat',
-          'https://mstdn.jp/@himarori'
-        ])
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,7 +33,8 @@ class _BookListScreenState extends State<BookListScreen> {
   }
 
   List<Widget> _getResults() {
-    final results = books
+    final results = DataProvider()
+        .books
         .map((book) => BookItem(book: book))
         .map<Widget>((item) => GestureDetector(
               onTap: () => NavigateProvider.push(
