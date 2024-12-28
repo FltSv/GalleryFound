b3f918113a0fc396aea8f97c9de6ddc82f5f23ba
suna <23271500+FltSv@users.noreply.github.com>
2024-12-28 16:00:44 +0900

    #161 package_infoをpackage_info_plusに更新

--- mobile/lib/services/version_service.dart
+++ mobile/lib/services/version_service.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:mobile/providers/config_provider.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pub_semver/pub_semver.dart';
 import 'package:url_launcher/url_launcher.dart';
 
