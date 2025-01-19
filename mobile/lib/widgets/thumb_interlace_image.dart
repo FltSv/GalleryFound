25e165c088294a2f764839392751a5986d5b1388
suna <23271500+FltSv@users.noreply.github.com>
2025-01-19 16:02:41 +0900

    #186 画像表示時のラグを改善

--- mobile/lib/widgets/thumb_interlace_image.dart
+++ mobile/lib/widgets/thumb_interlace_image.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/models/image_base.dart';
-import 'package:mobile/widgets/loading_placeholder.dart';
 import 'package:mobile/widgets/thumb_image.dart';
 
 class ThumbInterlaceImage extends StatelessWidget {
@@ -16,41 +15,29 @@ class ThumbInterlaceImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final blurColor = Theme.of(context).colorScheme.surface;
+    final blurColor = Theme.of(context).colorScheme.surface.withOpacity(0.2);
 
-    return FutureBuilder(
-      future: imageBase.thumbUrl,
-      builder: (context, snapshot) {
-        final resolvedThumbnailUrl = snapshot.data;
-        final availThumb =
-            resolvedThumbnailUrl != null && resolvedThumbnailUrl.isNotEmpty;
-
-        return CachedNetworkImage(
-          imageUrl: imageBase.imageUrl,
-          placeholder: (context, url) => availThumb
-              ? FittedBox(
-                  child: Stack(
-                    children: [
-                      ThumbImage(imageBase: imageBase),
-                      Positioned.fill(
-                        child: Stack(
-                          children: [
-                            BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-                              child:
-                                  Container(color: blurColor.withOpacity(0.2)),
-                            ),
-                            const Center(child: CircularProgressIndicator()),
-                          ],
-                        ),
-                      ),
-                    ],
+    return CachedNetworkImage(
+      imageUrl: imageBase.imageUrl,
+      placeholder: (context, url) => FittedBox(
+        child: Stack(
+          children: [
+            ThumbImage(imageBase: imageBase),
+            Positioned.fill(
+              child: Stack(
+                children: [
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                    child: Container(color: blurColor),
                   ),
-                )
-              : const LoadingPlaceholder(),
-          errorWidget: (context, url, error) => const Icon(Icons.error),
-        );
-      },
+                  const Center(child: CircularProgressIndicator()),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
     );
   }
 }
