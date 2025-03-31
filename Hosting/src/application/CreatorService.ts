b518fa4687e64daa34092dd172b045b6ca57988a
suna <23271500+FltSv@users.noreply.github.com>
2025-03-31 19:16:00 +0900

    #233 画像削除処理の分離

--- Hosting/src/application/CreatorService.ts
+++ Hosting/src/application/CreatorService.ts
@@ -56,6 +56,14 @@ export const updateProduct = async (
   await FirestoreCreatorRepo.updateProduct(userId, product);
 };
 
+export const deleteProduct = async (userId: string, product: Product) => {
+  const deleteTasks = [
+    FirestoreCreatorRepo.deleteProduct(userId, product),
+    imageService.deleteImage(product),
+  ];
+  await Promise.all(deleteTasks);
+};
+
 export const createExhibit = async (
   userId: string,
   exhibitData: ExhibitData,
@@ -117,6 +125,14 @@ export const updateExhibit = async (
   };
   await FirestoreCreatorRepo.updateExhibit(userId, imageUpdatedExhibit);
   return imageUpdatedExhibit;
+};
+
+export const deleteExhibit = async (userId: string, exhibit: Exhibit) => {
+  const deleteTasks = [
+    FirestoreCreatorRepo.deleteExhibit(userId, exhibit),
+    imageService.deleteImage(exhibit),
+  ];
+  await Promise.all(deleteTasks);
 };
 
 const parseSrcImage = (imageUrl: string) =>
